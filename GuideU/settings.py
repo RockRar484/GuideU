90cc094dac41eabf6c0737021bb3f4e80dd06969
RockRar484 <czileen484patel@gmail.com>
2024-06-20 23:12:39 +0530

    static files added

--- GuideU/settings.py
+++ GuideU/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "client-side/out"),
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -156,6 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ client-side/next.config.js
@@ -0,0 +1,5 @@
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    output: 'export', // This option is valid and necessary for static export
+};
