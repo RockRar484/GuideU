430bb8b5e37ad45665ef577be843e68020b943df
Aditya Uttarwar <136005005+DeAdiU@users.noreply.github.com>
2024-07-21 00:40:08 +0530

    Update Navbar.jsx

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ import { useAuth } from '@/context/AuthContext'
 import { Button } from '@/components/ui/button';
 import { MoreHorizontal } from 'lucide-react'
 import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 const Navbar = () => {
     const [token, setToken] = useState(null);
@@ -32,30 +32,32 @@ const Navbar = () => {
     return (
         <>
             <header>
+            
                 <input type="checkbox" name="hbr" id="hbr" className="hbr peer" hidden aria-hidden="true" />
                 <nav className="fixed z-20 w-full bg-white/80 dark:bg-gray-900/80 backdrop-blur navbar shadow-md shadow-gray-600/5 peer-checked:navbar-active md:relative md:bg-transparent dark:shadow-none">
                     <div className="xl:container m-auto px-6 md:px-12">
                         <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0">
                             <div className="w-full flex justify-between lg:w-auto">
-                                <a href="#" aria-label="logo" className="flex space-x-2 items-center">
+                                <div onClick={() => router.push('/')} role="button" tabIndex={0} aria-label="logo" className="flex space-x-2 items-center">
                                     <div aria-hidden="true" className="flex space-x-1">
                                         <div className="h-4 w-4 rounded-full bg-gray-900 dark:bg-gray-200"></div>
                                         <div className="h-6 w-2 bg-primary dark:bg-primaryLight"></div>
                                     </div>
                                     <span className="text-base font-bold text-gray-600 dark:text-white ml-1">GuideU</span>
-                                </a>
+                                </div>
                                 <label htmlFor="hbr" className="peer-checked:hamburger block relative z-20 p-6 -mr-6 cursor-pointer lg:hidden">
                                     <div aria-hidden="true" className="m-auto h-0.5 w-6 rounded bg-gray-900 dark:bg-gray-300 transition duration-300"></div>
                                     <div aria-hidden="true" className="m-auto mt-2 h-0.5 w-6 rounded bg-gray-900 dark:bg-gray-300 transition duration-300"></div>
                                 </label>
+                                
                             </div>
-
-                            <div className="navmenu hidden w-auto flex-wrap justify-end items-center mb-16 space-y-8 p-6 border border-gray-100 rounded-3xl shadow-2xl shadow-gray-300/20 bg-white dark:bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none dark:shadow-none dark:border-gray-700 lg:border-0">
-                                <ModeToggle />
+                            
+                            
+                            <div className="navmenu hidden w-full flex-wrap justify-center items-center mb-16 space-y-8 p-6 border border-gray-100 rounded-3xl shadow-2xl shadow-gray-300/20 bg-white dark:bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none dark:shadow-none dark:border-gray-700 lg:border-0">
                                 <div className="text-gray-600 dark:text-gray-300 lg:pr-4">
-                                    <ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0">
+                                    <ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0 justify-center items-center text-center">
                                         <li>
-                                            <div onClick={() => router.push('/')} className="block md:px-4 transition hover:cursor-pointer hover:text-primary dark:hover:text-primaryLight">
+                                            <div onClick={() => router.push('/')} className="block md:px-4  transition hover:cursor-pointer hover:text-primary dark:hover:text-primaryLight">
                                                 <span>Home</span>
                                             </div>
                                         </li>
@@ -72,43 +74,43 @@ const Navbar = () => {
                                                 <span>News</span>
                                             </div>
                                         </li>
-                                        {isLoggedIn ? null : (<li>
+                                       {isLoggedIn ? null :( <li>
                                             <div onClick={() => router.push('/about')} className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
                                                 <span>About Us</span>
                                             </div>
                                         </li>)}
                                     </ul>
                                 </div>
-                                {isLoggedIn ? (
+                                { isLoggedIn ? (
                                     <DropdownMenu>
-                                        <DropdownMenuTrigger asChild>
-                                            <div onClick={() => router.push('/colleges')} className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
-                                                <Button variant="ghost" className="h-8 w-8 p-0">
-                                                    <span className="sr-only">Open menu</span>
-                                                    <MoreHorizontal className="h-4 w-4" />
-                                                </Button>
-                                            </div>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent align="end">
-                                            <DropdownMenuItem
-                                                onClick={() => router.push('/about')}
-                                            >
-                                                About Us
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem
-                                                onClick={() => router.push('/analysis')}
-                                            >
-                                                Yearly Trend
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem
-                                                onClick={() => router.push('/topbranches')}
-                                            >
-                                                Top Branches
-                                            </DropdownMenuItem>
-                                            <DropdownMenuSeparator />
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                ) : null}
+                                    <DropdownMenuTrigger asChild>
+                                    <div onClick={() => router.push('/colleges')} className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
+                                      <Button variant="ghost" className="h-8 w-8 p-0">
+                                        <span className="sr-only">Open menu</span>
+                                        <MoreHorizontal className="h-4 w-4" />
+                                      </Button>
+                                      </div>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end">
+                                      <DropdownMenuItem
+                                        onClick={() => router.push('/about')}
+                                      >
+                                        About Us
+                                      </DropdownMenuItem>
+                                      <DropdownMenuItem
+                                        onClick={() => router.push('/analysis')}
+                                      >
+                                        Yearly Trend
+                                      </DropdownMenuItem>
+                                      <DropdownMenuItem
+                                        onClick={() => router.push('/topbranches')}
+                                      >
+                                        Top Branches   
+                                      </DropdownMenuItem>
+                                      <DropdownMenuSeparator />
+                                    </DropdownMenuContent>
+                                  </DropdownMenu>
+                                ): null}
                                 {isLoggedIn ?
                                     (
                                         <div className="w-full pl-3 space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
