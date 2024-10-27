a4c624f03943f5c45ace5b2bdc91a2de378d2c32
DeAdiU <uttarwaraditya29@gmail.com>
2024-10-27 20:30:53 +0530

    vercel deploy and db changed

--- GuideU/settings.py
+++ GuideU/settings.py
@@ -119,11 +119,14 @@ WSGI_APPLICATION = 'GuideU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guideu',
+        'USER': 'eprinter_owner',
+        'PASSWORD':'Yfolyxzg48WR',
+        'HOST':'ep-crimson-surf-a1weguwe.ap-southeast-1.aws.neon.tech',
+        'PORT':'5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
